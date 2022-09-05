512459ddec9b51b2ad88215bb9ead72618e29121
kronos44 <58785351+kronos44@users.noreply.github.com>
2022-09-05 23:29:11 +0300

    Add files via upload

---
+++ basic_op/main2.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.22.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "tf-ec2" {
+  ami           = "ami-0ed9277fb7eb570c9"
+  instance_type = "t2.micro"
+  key_name      = "mk"    # write your pem file without .pem extension>
+  tags = {
+    "Name" = "tf-ec2"
+  }
+}
+
+resource "aws_s3_bucket" "tf-s3" {
+  bucket = "oliver-tf-test-bucket-addwhateveryouwant"
+}
